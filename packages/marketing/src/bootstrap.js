770ca276a38fe3abc0479abd1b71c51973ed0258
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2025-10-19 21:10:24 +0700

    Created pagge navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,14 +1,31 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-function mount(el) {
-  ReactDOM.render(<App />, el);
+function mount(el, { onNavigate, defaultHistory }) {
+  const history = defaultHistory ?? createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 }
 
 if (process.env.NODE_ENV == "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
-  if (devRoot) mount(devRoot);
+  if (devRoot) mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 export { mount };
